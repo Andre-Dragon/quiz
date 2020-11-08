23b2d6df92defc367b72d6524c7590362d75a10d
Andre-Dragon <andrey_alekseev_1982@inbox.ru>
2020-11-08 17:44:34 +0300

    JSON was added

--- source/js/main.js
+++ source/js/main.js
@@ -14,25 +14,24 @@ document.addEventListener('DOMContentLoaded', () => {
     step0: {
       question: '',
       answers: [],
-
     },
-
     step1: {
       question: '',
       answers: [],
-
     },
-
     step2: {
       question: '',
       answers: [],
-
     },
-
     step3: {
-      questio: '',
+      question: '',
       answers: [],
-
+    },
+    step4: {
+      name: '',
+      phone: '',
+      email: '',
+      call: '',
     },
   };
 
@@ -78,6 +77,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (formItemIndex !== formItems.length - 1) {
       const inputs = formItem.querySelectorAll('input');
+      const formTitle = formItem.querySelector('.form__title');
+
+      answersObj[`step${formItemIndex}`].question = formTitle.textContent;
 
       inputs.forEach(input => {
         const parent = input.parentNode;
@@ -91,30 +93,81 @@ document.addEventListener('DOMContentLoaded', () => {
     formItem.addEventListener('change', event => {
       const target = event.target;
       const inputsChecked = formItem.querySelectorAll('input:checked');
-      
-      if (inputsChecked.length > 0) {
-        btnsNext[formItemIndex].disabled = false;
-      } else {
-        btnsNext[formItemIndex].disabled = true;
-      }
 
-      if (target.classList.contains('input-radio')) {
-        const inputRadio = formItem.querySelectorAll('.input-radio');
-        inputRadio.forEach(input => {
-
-          if (input === target) {
-            input.parentNode.classList.add('active-radio');
-          } else {
-            input.parentNode.classList.remove('active-radio');
-          }
-
+      if (formItemIndex !== formItems.length - 1) {
+        answersObj[`step${formItemIndex}`].answers.length = 0;
+        inputsChecked.forEach(inputChecked => {
+          answersObj[`step${formItemIndex}`].answers.push(inputChecked.value);
         });
-      } else {
-        return;
+        
+        if (inputsChecked.length > 0) {
+          btnsNext[formItemIndex].disabled = false;
+        } else {
+          btnsNext[formItemIndex].disabled = true;
+        }
+
+        if (target.classList.contains('input-radio')) {
+          const inputRadio = formItem.querySelectorAll('.input-radio');
+          inputRadio.forEach(input => {
+
+            if (input === target) {
+              input.parentNode.classList.add('active-radio');
+            } else {
+              input.parentNode.classList.remove('active-radio');
+            }
+
+          });
+        } else {
+          return;
+        }
       }
     });
-
   });
+
+  const postData = body => {
+    return fetch('./server.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+  };
+
+  const sendForm = () => {
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+
+      answersObj.step4.name = document.getElementById('quiz-name').value;
+      answersObj.step4.phone = document.getElementById('quiz-phone').value;
+      answersObj.step4.email = document.getElementById('quiz-email').value;
+      answersObj.step4.call = document.getElementById('quiz-call').value;
+
+      // for (let key in answersObj.step4) {
+      //   if (answersObj.step4[key].value === "") {
+      //     alert("Введите даные во все поля");
+      //   }
+      // }
+
+      if (document.getElementById('quiz-policy').checked) {
+        postData(answersObj)
+          .then((res) => res.json())
+          .then((res) => {
+            if (res.status === 'ok') {
+              overlay.classList.add('hide');
+              quiz.classList.add('hide');
+              form.reset();
+              alert(res.message);
+            } else if (res.status === 'error') {
+              alert(res.message);
+            }
+          });
+      } else {
+        alert('Дайте согласие на обработку персональных данных');
+      }
+     
+    });
+  };
 
   overlay.classList.add('hide');
   quiz.classList.add('hide');
@@ -122,7 +175,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   passTestButton.addEventListener('click', () => {
     overlay.classList.remove('hide');
-  quiz.classList.remove('hide');
+    quiz.classList.remove('hide');
   });
 
+  sendForm();
 });
